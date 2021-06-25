bb499ce2727a6bc65f41850996bdb1a148143841
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-06-25 14:47:59 -0400

    Add files via upload

---
+++ Assignment_6/API_APP/src/app/artist-discography.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MusicDataService } from '../music-data.service';
+
+@Component({
+  selector: 'app-artist-discography',
+  templateUrl: './artist-discography.component.html',
+  styleUrls: ['./artist-discography.component.css']
+})
+export class ArtistDiscographyComponent implements OnInit {
+  id:string;
+  
+  public artist_info:Artist[]=[];
+  public artists_full:Array<any>=[];
+  constructor(private _route: ActivatedRoute,private _artist:MusicDataService){ 
+    this.id = this._route.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this._artist.getArtistById(this.id).subscribe(res=>{
+       this.artist_info.push({
+         name:res.name,
+         image:res.images[0].url
+       });
+      
+    })
+
+    this._artist.getAlbumsByArtistId(this.id).subscribe(res=>{
+        Object.keys(res.items).map(key => {
+        this.artists_full.push(
+          {"name":res.items[key].name,
+           "image":res.items[key].images[1],
+           "release_date":res.items[key].release_date,
+           "total_tracks":res.items[key].total_tracks,
+           "album_id":res.items[key].id
+          });
+      })
+    }); //end of getalbumbyartistid
+  }
+
+}
+export interface Artist{
+  name:string;
+  image:string;
+}
