5e472e3b8aebe626b56cd1855b6f3f28eafaab56
aimee718 <46436904+aimee718@users.noreply.github.com>
2021-06-25 14:33:04 -0400

    Add files via upload

---
+++ Assignment_6/API_APP/src/app/album.component.ts
@@ -0,0 +1,94 @@
+import { mergeMap } from 'rxjs/operators';
+import { Component, OnInit,Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { MusicDataService } from '../music-data.service';
+
+
+
+@Component({
+  selector: 'app-album',
+  templateUrl: './album.component.html',
+  styleUrls: ['./album.component.css']
+})
+export class AlbumComponent implements OnInit {
+  @Input() received: string; 
+
+  id:string;
+  public aid:string;
+  data99:Array<any>=[];
+  data77:string;
+  public data88:Album[]=[];
+  public tr_items:Array<any>=[];
+
+
+  constructor(private _route: ActivatedRoute,private snackBar:MatSnackBar
+    ,private _albums:MusicDataService){ 
+    this.id = this._route.snapshot.params['id'];
+   }
+  
+ 
+  ngOnInit(): void {
+    this._albums.getAlbumById(this.id)
+            .subscribe(res=>{
+            this.data99 =res.name;
+            this.data77 =res.images[1].url; 
+           
+              this.data88.push(
+                {
+                  name:res.name,
+                  image:res.images[1].url,
+                  label:res.label,
+                  release_date:res.release_date,
+                  total_tracks:res.total_tracks,
+                  popularity:res.popularity,
+                  artistname:res.tracks.items[0].artists[0].name,
+                  copyrights:res.copyrights[0].text,
+                  art_id:res.artists[0].id                
+                }
+              );
+              
+               Object.keys(res.tracks.items).map(key => {
+                this.tr_items.push(
+                  {"track_no":res.tracks.items[key].track_number,
+                   "track_name":res.tracks.items[key].name,
+                   "duration_ms":res.tracks.items[key].duration_ms,
+                   "trackID":res.tracks.items[key].id,
+                   "preview_url":res.tracks.items[key].preview_url
+                  });
+              })
+              
+    })//end of subscribe
+   
+    this._albums.getNewReleases().subscribe(res2=>{
+      Object.keys(res2.albums.items).map(key => {
+        if(res2.albums.items[key].id == this.id)
+        this.aid = res2.albums.items[key].artists[0].id;
+      })
+    })
+    
+  }//end of ngOnInt
+  ArrayFav:Array<any>=[];
+  addFav(trackid:any){
+   this._albums.addToFavourites(trackid)
+   .subscribe(resp => {
+    return this.ArrayFav.push(resp);
+   });
+    this.snackBar.open("Adding to Favourites...", "Done", { duration: 1500 }); 
+    
+  }
+
+}
+export interface Album {
+  name: string; 
+  image: string; 
+  label: string;
+  release_date:string;
+  total_tracks:number;
+  popularity:number;
+  artistname:string;
+  copyrights:string;
+  art_id:string;
+ 
+}
+
